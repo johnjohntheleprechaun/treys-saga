5538a88a6db08915b12f6cc2627fd59ec97bcb1c
John Jorgensen <john@jorgensenfamily.us>
2024-02-22 00:03:58 -0800

    add comic display code

--- comics/index.js
+++ comics/index.js
@@ -1,3 +1,22 @@
-window.addEventListener("load", () => {
+const comics = [];
+let comicDB;
+
+window.addEventListener("load", async () => {
     console.log("hello world");
-})
+    await loadComics();
+});
+
+async function loadComics() {
+    comicDB = await fetch("/comics.json").then(a=>a.json());
+    for (const key in comicDB) {
+        const comic = new Comic(key);
+        comics.push(comic)
+        document.body.appendChild(comic);
+    }
+    const levels = populateLevels(comics);
+    let i = 0;
+    for (const level of levels) {
+        displayCircle(level, 125 * i);
+        i++;
+    }
+}
