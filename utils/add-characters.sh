b3ceae9c6799811992fec68881ccac62b69a091c
John Jorgensen <john@jorgensenfamily.us>
2024-02-23 04:17:24 -0800

    switch add character to be a loop

--- utils/add-characters.sh
+++ utils/add-characters.sh
@@ -1,9 +1,12 @@
 CURRENT=$(cat characters.json)
 
+while true; do
 read -p "Characters Name: " NAME
 
 NEWFILE=$(echo $CURRENT | jq \
-    --arg name $NAME \
+    --arg name "$NAME" \
     '. + [{ name: $name, pfp: "", comics: [] }]'
 )
 echo -E "$NEWFILE" > characters.json
+CURRENT=$NEWFILE
+done;
