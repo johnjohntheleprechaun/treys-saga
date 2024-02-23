ae272083fd9a3c7b2f4bbad3ca42d86f1c767844
John Jorgensen <john@jorgensenfamily.us>
2024-02-22 21:42:44 -0800

    node code cleanup

---
+++ utils/node-events.js
@@ -0,0 +1,25 @@
+let mouseButton = -1;
+
+window.addEventListener("click", event => {
+    if (event.target === document.body || event.target === document.documentElement) {
+        unfocusAll();
+    }
+});
+window.addEventListener("mousemove", event => {
+    if (mouseButton === -1) {
+        return;
+    }
+    if (event.target === document.body || event.target === document.documentElement || mouseButton === 1) {
+        setOffset(offsetX + event.movementX, offsetY + event.movementY);
+    }
+});
+window.addEventListener("mousedown", e => {
+    e.preventDefault();
+    mouseButton = e.button
+    
+});
+window.addEventListener("mouseup", _ => { mouseButton = -1 });
+window.addEventListener("wheel", event => {
+    console.log(event.deltaY);
+    setScale(scale - event.deltaY * scalingModifier);
+});
