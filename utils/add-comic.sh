546b8e9f2b249686992d18ce10a38aa924210b79
John Jorgensen <john@jorgensenfamily.us>
2024-02-22 22:45:52 -0800

    add urls to comics db

--- utils/add-comic.sh
+++ utils/add-comic.sh
@@ -4,7 +4,8 @@ EMBED=$(curl -s https://www.reddit.com/oembed?url=$1)
 echo "$EMBED\n\n\n"
 NEWFILE=$(echo $CURRENT | jq \
     --arg uuid $UUID \
+    --arg url $1 \
     --argjson embed "$EMBED" \
-    '. + {($uuid): {title: $embed.title, embedCode: $embed.html}}'
+    '. + {($uuid): {title: $embed.title, url: $url, embedCode: $embed.html}}'
 )
 echo -E "$NEWFILE" > comics.json
