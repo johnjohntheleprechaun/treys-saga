7acb7d1b267028c5f6e366ea16c135a80ff2cd5f
John Jorgensen <john@jorgensenfamily.us>
2024-02-23 04:22:04 -0800

    add description to character add script

--- utils/add-characters.sh
+++ utils/add-characters.sh
@@ -2,10 +2,12 @@ CURRENT=$(cat characters.json)
 
 while true; do
 read -p "Characters Name: " NAME
+read -p "Character Description: " DESCRIPTION
 
 NEWFILE=$(echo $CURRENT | jq \
     --arg name "$NAME" \
-    '. + [{ name: $name, pfp: "", comics: [] }]'
+    --arg desc "$DESCRIPTION" \
+    '. + [{ name: $name, description: $desc, pfp: "", comics: [] }]'
 )
 echo -E "$NEWFILE" > characters.json
 CURRENT=$NEWFILE
